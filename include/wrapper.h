946e5238243deabc0c5e6e8701df4422276b0a68
Tine Zata <60703214+TineZata@users.noreply.github.com>
2025-04-01 16:30:18 +0100

    added rust implementation for ClientProvider and ClientChannel

--- include/wrapper.h
+++ include/wrapper.h
@@ -3,20 +3,15 @@
 #include <rust/cxx.h>  // Required for Rust interop
 #include <pva/client.h>
 
-rust::String get_pv_value(rust::Str name);
 
-class PvaClientChannel {
-    public:
-        // Constructor
-        PvaClientChannel(std::string name);
-    
-        // Connect method
-        //bool connect();
-    
-        // Factory method to create and return a connected channel
-        static std::unique_ptr<PvaClientChannel> create_connected(rust::Str name);
-    
-    private:
-        std::unique_ptr<pvac::ClientProvider> provider;  // Store the provider
-        std::unique_ptr<pvac::ClientChannel> channel;    // Store the channel
-    };
+// Global map to store ClientProvider instances
+extern std::unique_ptr<pvac::ClientProvider> rust_client_provider; 
+// Global map to store ClientChannel instances
+extern std::unique_ptr<pvac::ClientChannel> rust_client_channel; 
+
+// Function to create a ClientProvider
+std::unique_ptr<pvac::ClientProvider> get_client_provider();
+// Function to create a ClientProvider
+std::unique_ptr<pvac::ClientChannel> get_client_channel(rust::Str name);
+// Function to get a PV value
+rust::String get_pv_value(rust::Str name);
