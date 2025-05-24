eacf232536ccf40628a0a12ce3dbbe71ae27e1f1
Tine Zata <60703214+TineZata@users.noreply.github.com>
2025-05-25 00:47:46 +0100

    added nt_enum_get_value_index and nt_enum_get_value_choices

--- src/wrapper_nt_enum.cpp
+++ src/wrapper_nt_enum.cpp
@@ -1,3 +1,6 @@
+#include "wrapper.h"
+#include "helpers.h"
+
 /*
 epics:nt/NTEnum:1.0
         enum_t value
@@ -12,3 +15,46 @@ epics:nt/NTEnum:1.0
             int nanoseconds
             int userTag
 */
+
+int nt_enum_get_value_index(std::shared_ptr<PVStructure> pvStructureSharedPtr) {
+    int value_index = -1; // Default value if not found
+    
+    try {
+        std::shared_ptr<EpicsNtNTEnum> enumWrapper = EpicsNtNTEnum::wrap(pvStructureSharedPtr);
+        if (enumWrapper.get() != 0) {
+            // Extract the value index from the NTEnum structure
+            value_index = enumWrapper->getValue()->getSubField<epics::pvData::PVInt>("index")->get();
+        } else {
+            std::cerr << "Invalid NTEnum structure." << std::endl;
+            return int(-1);
+        }
+    } catch (std::exception &e) {
+        // Handle exceptions and clean up
+        std::cerr << "Error extracting NTEnum: " << e.what() << std::endl;
+        return int(-1);
+    }
+    return value_index;
+}
+
+const char* const* nt_enum_get_value_choices(std::shared_ptr<PVStructure> pvStructureSharedPtr) {
+    const char* const* value_choices = nullptr;
+
+    try {
+        std::shared_ptr<EpicsNtNTEnum> enumWrapper = EpicsNtNTEnum::wrap(pvStructureSharedPtr);
+        if (enumWrapper.get() != 0) {
+            auto choicesField = enumWrapper->getValue()->getSubField<epics::pvData::PVValueArray<std::string>>("choices");
+            auto castedChoicesField = std::dynamic_pointer_cast<const epics::pvData::PVStringArray>(choicesField);
+            if (castedChoicesField) {
+                value_choices = extract_string_array(castedChoicesField).data();
+            }
+        } else {
+            std::cerr << "Invalid NTEnum structure." << std::endl;
+            return nullptr;
+        }
+    } catch (std::exception &e) {
+        // Handle exceptions and clean up
+        std::cerr << "Error extracting NTEnum: " << e.what() << std::endl;
+        return nullptr;
+    }
+    return value_choices;
+}
